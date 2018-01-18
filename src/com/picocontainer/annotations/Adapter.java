dfe14b9ecc4d11a0166af02587dd367a44a85614
chaulin <chaulin@haulmont.com>
2018-01-18 14:05:40 +0400

    -

---
+++ src/com/picocontainer/annotations/Adapter.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2008 - 2018 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+ */
+
+package com.picocontainer.annotations;
+
+public @interface Adapter {
+}
--- src/com/picocontainer/annotations/Component.java
+++ src/com/picocontainer/annotations/Component.java
@@ -1,11 +1,4 @@
-/*
- * Copyright 2008 - 2017 Haulmont Technology Ltd. All Rights Reserved.
- * Haulmont Technology proprietary and confidential.
- * Use is subject to license terms.
- */
-
 package com.picocontainer.annotations;
-
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -17,5 +10,5 @@ import java.lang.annotation.Target;
 public @interface Component {
     String name() default "";
     Class<?> impl() default void.class;
+    Class<?> dependsOn() default void.class;
 }
-
